95945feaf1e4c6f6d339018598be1b8a2c589251
Victor Agbeo <75548415+saintvic@users.noreply.github.com>
2022-03-26 17:01:25 -0500

    Express Session P1

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -31,10 +33,19 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+//Middle-Wares
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
+app.use(session({
+  name:'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
+// cant use cookie parser with express cookies.
+// app.use(cookieParser('12345-67890-09876-54321'));
 /* middleware are applied in order so its crucial
 to apply Authentication b4 they access data from server
 */
